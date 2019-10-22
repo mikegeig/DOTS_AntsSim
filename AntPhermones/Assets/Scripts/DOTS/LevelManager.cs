320905d36a8d614a74de39723342863c4c207bfa
belivo <56844569+belivo@users.noreply.github.com>
2019-10-22 10:25:08 -0400

    Added bucket generation part 1

--- AntPhermones/Assets/Scripts/DOTS/LevelManager.cs
+++ AntPhermones/Assets/Scripts/DOTS/LevelManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Collections;
+using Unity.Entities;
 
 public class LevelManager : MonoBehaviour
 {
@@ -22,12 +24,94 @@ public class LevelManager : MonoBehaviour
 
 	Matrix4x4 resourceMatrix;
 	Matrix4x4 colonyMatrix;
+    public int bucketResolution;
+	const int instancesPerBatch = 1023;
 
 	public int obstacleRingCount;
 	[Range(0f,1f)]
 	public float obstaclesPerRing;
 	public float obstacleRadius;
 	
+	NativeArray<Obstacle> obstacles;
+	Matrix4x4[][] obstacleMatrices;
+	Obstacle[,][] obstacleBuckets;
+
+	void GenerateObstacles() {
+		List<Obstacle> output = new List<Obstacle>();
+		for (int i=1;i<=obstacleRingCount;i++) {
+			float ringRadius = (i / (obstacleRingCount+1f)) * (mapSize * .5f);
+			float circumference = ringRadius * 2f * Mathf.PI;
+			int maxCount = Mathf.CeilToInt(circumference / (2f * obstacleRadius) * 2f);
+			int offset = Random.Range(0,maxCount);
+			int holeCount = Random.Range(1,3);
+			for (int j=0;j<maxCount;j++) {
+				float t = (float)j / maxCount;
+				if ((t * holeCount)%1f < obstaclesPerRing) {
+					float angle = (j + offset) / (float)maxCount * (2f * Mathf.PI);
+					Obstacle obstacle = new Obstacle();
+					obstacle.position = new Vector2(mapSize * .5f + Mathf.Cos(angle) * ringRadius,mapSize * .5f + Mathf.Sin(angle) * ringRadius);
+					obstacle.radius = obstacleRadius;
+					output.Add(obstacle);
+					//Debug.DrawRay(obstacle.position / mapSize,-Vector3.forward * .05f,Color.green,10000f);
+				}
+			}
+		}
+
+		obstacleMatrices = new Matrix4x4[Mathf.CeilToInt((float)output.Count / instancesPerBatch)][];
+		for (int i=0;i<obstacleMatrices.Length;i++) {
+			obstacleMatrices[i] = new Matrix4x4[Mathf.Min(instancesPerBatch,output.Count - i * instancesPerBatch)];
+			for (int j=0;j<obstacleMatrices[i].Length;j++) {
+				obstacleMatrices[i][j] = Matrix4x4.TRS(output[i * instancesPerBatch + j].position / mapSize,Quaternion.identity,new Vector3(obstacleRadius*2f,obstacleRadius*2f,1f)/mapSize);
+			}
+		}
+
+	    obstacles = new NativeArray<Obstacle>(output.ToArray() , Allocator.Persistent);
+
+		List<Obstacle>[,] tempObstacleBuckets = new List<Obstacle>[bucketResolution,bucketResolution];
+
+		for (int x = 0; x < bucketResolution; x++) {
+			for (int y = 0; y < bucketResolution; y++) {
+				tempObstacleBuckets[x,y] = new List<Obstacle>();
+			}
+		}
+
+		for (int i = 0; i < obstacles.Length; i++) {
+			Vector2 pos = obstacles[i].position;
+			float radius = obstacles[i].radius;
+			for (int x = Mathf.FloorToInt((pos.x - radius)/mapSize*bucketResolution); x <= Mathf.FloorToInt((pos.x + radius)/mapSize*bucketResolution); x++) {
+				if (x < 0 || x >= bucketResolution) {
+					continue;
+				}
+				for (int y = Mathf.FloorToInt((pos.y - radius) / mapSize * bucketResolution); y <= Mathf.FloorToInt((pos.y + radius) / mapSize * bucketResolution); y++) {
+					if (y<0 || y>=bucketResolution) {
+						continue;
+					}
+					tempObstacleBuckets[x,y].Add(obstacles[i]);
+				}
+			}
+		}
+
+		obstacleBuckets = new Obstacle[bucketResolution,bucketResolution][];
+		for (int x = 0; x < bucketResolution; x++) {
+			for (int y = 0; y < bucketResolution; y++) {
+				obstacleBuckets[x,y] = tempObstacleBuckets[x,y].ToArray();
+			}
+		}
+	}
+
+	Obstacle[] emptyBucket = new Obstacle[0];
+	Obstacle[] GetObstacleBucket(Vector2 pos) {
+		return GetObstacleBucket(pos.x,pos.y);
+	}
+	Obstacle[] GetObstacleBucket(float posX, float posY) {
+		int x = (int)(posX / mapSize * bucketResolution);
+		int y = (int)(posY / mapSize * bucketResolution);
+		if (x<0 || y<0 || x>=bucketResolution || y>=bucketResolution) {
+			return emptyBucket;
+		} else {
+			return obstacleBuckets[x,y];
+		}
+	}
 
 	void Awake()
     {
@@ -45,6 +129,13 @@ public class LevelManager : MonoBehaviour
 		float resourceAngle = Random.value * 2f * Mathf.PI;
 		resourcePosition = Vector2.one * mapSize * .5f + new Vector2(Mathf.Cos(resourceAngle) * mapSize * .475f, Mathf.Sin(resourceAngle) * mapSize * .475f);
 		resourceMatrix = Matrix4x4.TRS(resourcePosition / mapSize, Quaternion.identity, new Vector3(4f, 4f, .1f) / mapSize);
+	
+		GenerateObstacles();
+	}
+
+	void OnDestroy()
+	{
+		//obstacles.Destroy();
 	}
 
 	void Update()
